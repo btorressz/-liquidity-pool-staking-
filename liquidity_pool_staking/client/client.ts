0993ee6c9f35d83fa8568772a2ae5a34e8192207
Brandon Torres <44816281+btorressz@users.noreply.github.com>
2024-08-28 14:18:48 -0700

    Update client.ts

--- liquidity_pool_staking/client/client.ts
+++ liquidity_pool_staking/client/client.ts
@@ -129,4 +129,4 @@ main().catch(err => {
 /*
 console.log("My address:", program.provider.publicKey.toString());
 const balance = await program.provider.connection.getBalance(program.provider.publicKey);
-console.log(`My balance: ${balance / web3.LAMPORTS_PER_SOL} SOL`);*'
+console.log(`My balance: ${balance / web3.LAMPORTS_PER_SOL} SOL`);*'/
